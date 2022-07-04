3941918011501cff05ccecc84919dd2b02f7ab11
avlokha <avlokhashvili93@gmail.com>
2022-07-04 13:56:21 +0400

    add script

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
+const currentDay = new Date().getDay();
+console.log(currentDay);
+let currentDayWorld = "";
+switch (currentDay) {
+  case 0:
+    currentDayWorld = "კვირა";
+    break;
+  case 1:
+    currentDayWorld = "ორშაბათი";
+    break;
+  case 2:
+    currentDayWorld = "სამშაბათი";
+    break;
+  case 3:
+    currentDayWorld = "ოთხშაბათი";
+    break;
+  case 4:
+    currentDayWorld = "ხუთშაბათი";
+    break;
+  case 5:
+    currentDayWorld = "პარასკევი";
+    break;
+  case 6:
+    currentDayWorld = "შაბათი";
 
+  default:
+    break;
+}
+
+console.log(currentDayWorld);
